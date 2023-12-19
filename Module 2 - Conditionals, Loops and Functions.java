4d36d13c91f50fb84eb9214f6c9f35dc30b8d1c2
LOKESH RAI <146547190+Mrlokeshrai20@users.noreply.github.com>
2023-12-19 18:03:24 +0530

    Update Module 2 - Conditionals, Loops and Functions.java

--- Module 2 - Conditionals, Loops and Functions.java
+++ Module 2 - Conditionals, Loops and Functions.java
@@ -12,9 +12,61 @@ Question 3: Write a Java program that prints the first N Fibonacci numbers using
 
 
 
-Create a Java program that calculates the factorial of a number using a while loop.
+Question 4: Create a Java program that calculates the factorial of a number using a while loop.
 
 
 
 
 Question 5: Write a Java function that takes an array of integers as input and returns the sum of all the even numbers in the array.
+
+
+Question 6: What is the output of the following code?
+ int x = 5;
+if (x > 0) {
+    for (int i = 0; i < x; i++) {
+        System.out.print(i + " ");
+    }
+} else {
+    System.out.println("Negative number");
+}
+
+a) 0 1 2 3 4
+b) 1 2 3 4 5
+c) Negative number
+d) Compilation error
+
+
+Question 7: How can you define a constant in Java?
+
+a) const int MY_CONSTANT = 10;
+b) constant MY_CONSTANT = 10;
+c) final int MY_CONSTANT = 10;
+d) static int MY_CONSTANT = 10;
+
+
+Question 8: What is the purpose of the continue statement in a loop?
+
+a) To exit the loop
+b) To skip the rest of the code inside the loop and move to the next iteration
+c) To restart the loop
+d) To check a condition
+
+
+Question 9: What is the output of the following code?
+  for (int i = 0; i < 5; i++) {
+    System.out.print(i + " ");
+}
+
+a) 0 1 2 3 4
+b) 1 2 3 4 5
+c) 0 1 2 3
+d) 1 2 3 4
+
+
+
+Question 10: How can you represent "not equal" in a conditional statement?
+
+a) =!
+b) !=
+c) <>
+d) ~=
