04227fcad2b31d4d3d0ff2e9ca3f96525fe8d977
LOKESH RAI <146547190+Mrlokeshrai20@users.noreply.github.com>
2023-12-19 18:21:25 +0530

    Update Module 3- Abstraction, Encapsulation & Inheritance.java

--- Module 3- Abstraction, Encapsulation & Inheritance.java
+++ Module 3- Abstraction, Encapsulation & Inheritance.java
@@ -25,3 +25,78 @@ and provide methods to interact with the objects.
 Question 5: In Java, how does inheritance contribute to polymorphism? Provide a Java code example illustrating a class 
 hierarchy with inheritance and demonstrate polymorphic behavior through method overriding.
 
+
+Question 6:What will be the output of the following Java code related to abstraction?
+    abstract class Shape {
+    abstract void draw();
+}
+
+class Circle extends Shape {
+    void draw() {
+        System.out.println("Drawing Circle");
+    }
+}
+
+public class AbstractionOutput1 {
+    public static void main(String[] args) {
+        Shape shape = new Circle();
+        shape.draw();
+    }
+}
+
+a) Drawing Circle
+b) Drawing Shape
+c) Compilation Error
+d) Runtime Error
+
+
+Question 7:What will be the output of the following Java code related to encapsulation?
+  class Employee {
+    private String name;
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+public class EncapsulationOutput2 {
+    public static void main(String[] args) {
+        Employee emp = new Employee();
+        emp.setName("John Doe");
+        System.out.println(emp.getName());
+    }
+}
+
+a) John Doe
+b) null
+c) Compilation Error
+d) Runtime Error
+
+
+Question 8: What is the purpose of the super keyword in Java?
+
+a) To call the superclass constructor
+b) To access the superclass members
+c) Both a and b
+d) Neither a nor b
+
+Question 9: Which of the following is an example of an abstract class?
+
+a) interface
+b) abstract interface
+c) abstract class
+d) concrete class
+
+Question 10: How does encapsulation contribute to code design in Java?
+
+a) By hiding implementation details
+b) By allowing multiple inheritances
+c) By providing default implementations
+d) By creating instances of classes
+
+  
+
