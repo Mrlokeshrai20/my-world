aa40c0e440a0b395ce32e6cb470d675249178600
LOKESH RAI <146547190+Mrlokeshrai20@users.noreply.github.com>
2023-12-19 20:12:36 +0530

    Update Module 3-Inheritance, composition.java

--- Module 3-Inheritance, composition.java
+++ Module 3-Inheritance, composition.java
@@ -45,4 +45,68 @@ d) Interfaces only
 
 
 
-  
+ 6. What is the output of the following code?
+  class Animal {
+    void makeSound() {
+        System.out.println("Animal sound");
+    }
+}
+
+class Dog extends Animal {
+    void makeSound() {
+        System.out.println("Dog barks");
+    }
+}
+
+a) Animal sound
+b) Dog barks
+c) Compilation Error
+d) Animal sound, Dog barks
+
+
+7. Inheritance in Java supports:
+
+a) Single inheritance
+b) Multiple inheritance
+c) Hybrid inheritance
+d) All of the above
+
+
+ 8:What is composition in Java?
+
+a) It is a type of inheritance
+b) It is a way to achieve polymorphism
+c) It is a design technique to create a complex object using simpler objects
+d) It is a way to achieve method overloading
+
+
+  9.What type of relationship does the Car class have with the Engine class?
+  class Engine {
+    void start() {
+        System.out.println("Engine started");
+    }
+}
+
+class Car {
+    Engine carEngine;
+
+    Car(Engine engine) {
+        carEngine = engine;
+    }
+
+    void startCar() {
+        carEngine.start();
+    }
+}
+a) Inheritance
+b) Association
+c) Composition
+d) Polymorphism
+
+
+10.In Java, what is the purpose of the super keyword?
+
+a) To call the superclass constructor
+b) To access the superclass members
+c) To instantiate an object
+d) To declare static methods
