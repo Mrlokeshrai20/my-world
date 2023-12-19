4d56a9b74569477a1fdf9bf0e9666524d7e6ecf4
LOKESH RAI <146547190+Mrlokeshrai20@users.noreply.github.com>
2023-12-19 16:30:52 +0530

    Create Module 3-Inheritance, composition.java

---
+++ Module 3-Inheritance, composition.java
@@ -0,0 +1,48 @@
+1. What is the output of the following code?
+String str1 = "hello";
+String str2 = new String("hello");
+System.out.println(str1 == str2);
+
+a) true
+
+b) false
+
+c) compilation error
+
+d) runtime error
+
+
+2.Which Java annotation is used to indicate that a method overrides a superclass method?
+
+a) @Override
+b) @Overload
+c) @OverrideMethod
+d) @Overriding
+
+
+3.Which of the following is an advantage of composition over inheritance?
+
+a) Simplicity and flexibility
+b) Code reusability
+c) Tighter coupling
+d) Easier to implement
+  
+
+4.In composition, what relationship exists between the classes involved?
+
+a) "is-a" relationship
+b) "has-a" relationship
+c) "extends" relationship
+d) "implements" relationship
+
+
+5.In Java, a class can inherit from:
+
+a) Multiple classes
+b) Single class only
+c) No class
+d) Interfaces only
+
+
+
+  
